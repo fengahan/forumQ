ca33034a9ea601a10aeeb17704f8182566826e72
desmond <1595798176@qq.com>
2019-11-14 17:41:02 +0800

    init config

---
+++ backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'LMUaq8vXI4CF7ARxtWv3c4b2TOOAJDUJ',
+        ],
+    ],
+];
---
+++ frontend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'K3Q1OA8ezh6CBLlSVmbKqdz38XDKnEgo',
+        ],
+    ],
+];

f6d1b714f113446aab490c3f2411bf5fb116c091
ahanfeng <1595798176@qq.com>
2020-05-07 10:32:08 +0800

    更新文件 update_ques.php

--- frontend/views/question/update_ques.php
+++ frontend/views/question/update_ques.php
@@ -144,7 +144,7 @@ $this->title = '更新问答' .'-'.Yii::$app->name;
     function createQues() {
         var form=document.getElementById("create_ques");
         var title=document.getElementsByName("title")[0].value;
-        var integral=document.getElementsByName("integral")[0].value;
+        var integral=document.getElementsByName("money")[0].value;
 
         document.getElementsByName("html_content")[0].value=editor.getPreviewedHTML();
 
